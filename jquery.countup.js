b5401694074d04e8fb20835be50617807087b7b9
Oğulcan Orhan <ogulcan@developer.(none)>
2011-12-06 03:19:06 +0200

    multiple instance support

--- jquery.countup.js
+++ jquery.countup.js
@@ -18,37 +18,52 @@
 	You should have received a copy of the GNU General Public License
 	along with this program; if not, write to the Free Software
 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- * * * * * */
+*/
 
 (function ( $ ) {
 
+	if (typeof Object.create !== 'function') {
+		Object.create = function (o) {
+			function F() {} 
+			F.prototype = o;
+			return new F();
+		};
+	}
+	
 	//example: $.countUp({ 'sinceDate':'01/01/2011', 'lang':'en', 'format':'day' });
 	//date format: dd/mm/yyyy-hh:mm:ss
 	//available langs: english (en), turkish (tr), deutsch (de), spanish (es)
 	//format options: full, day, seconds
 	
 	var _defaults = { 'sinceDate' : '17/10/2007-19:37:25', 'expiryDate' : 'now', 'lang': 'tr', 'format': 'seconds' , 'div_id' : '#jq_count_up', 'class_prefix' : '', 'seperator': ',', 'write_zero':'false'};
-	var _timeStructure = { 'year': '' ,'month': '' ,'day': '' ,'hour': '' ,'minute': '','seconds': ''};
+
+	var _timeStructure = { 'year': '', 'month': '', 'day': '', 'hour': '', 'minute': '', 'seconds': '' };
 	var _monthDayStructure = { 1: 31, 2:28, 3:31, 4:30, 5:31, 6:31, 7:30, 8:31, 9:30, 10:31, 11:30, 12:31 };	
-	var _lang = { 'en' : { 'year': 'years', 'month': 'months', 'day': 'days', 'hour': 'hours', 'minute': 'minutes', 'seconds': 'seconds',  },
-				  'tr' : { 'year': 'yıl', 'month': 'ay', 'day': 'gün', 'hour': 'saat', 'minute': 'dakika', 'seconds': 'saniye' },
-				  'es' : { 'year': 'año' ,'month': 'mes', 'day': 'día' ,'hour': 'horas' ,'minute': 'minuto','seconds': 'segundo'	},
-				  'de' : { 'year': 'jahr' ,'month': 'monat' ,'day': 'tag' ,'hour': 'stunde' ,'minute': 'minute','seconds': 'sekunden' }
-			    };
+	var _lang = { 
+			'en' : { 'year': 'years', 'month': 'months', 'day': 'days', 'hour': 'hours', 'minute': 'minutes', 'seconds': 'seconds',  },
+			'tr' : { 'year': 'yıl', 'month': 'ay', 'day': 'gün', 'hour': 'saat', 'minute': 'dakika', 'seconds': 'saniye' },
+			'es' : { 'year': 'año' ,'month': 'mes', 'day': 'día' ,'hour': 'horas' ,'minute': 'minuto','seconds': 'segundo'	},
+			'de' : { 'year': 'jahr' ,'month': 'monat' ,'day': 'tag' ,'hour': 'stunde' ,'minute': 'minute','seconds': 'sekunden' }
+		    };
 		    
 	var methods = {
-		//initialize
-		init : function (options) {		
-			if(options) { $.extend( _defaults, options ); }
-			if(_defaults['expiryDate'] == 'now') _defaults.expiryDate = new Date();
-			var _startDate = methods.getStartDate(_defaults.sinceDate);
-			var _endDate = methods.getExpiryDate(_defaults.expiryDate); 
-			methods.difference(_startDate, _endDate);		
+		
+		init : function (options) {
+			if(options) { var newOptions = $.extend({}, _defaults, options ); } 
+			if(newOptions['expiryDate'] == 'now') newOptions.expiryDate = new Date();
+
+			var _startDate = methods.getStartDateWithFormat(newOptions.sinceDate);
+			var _endDate = methods.getExpiryDate(newOptions.expiryDate);
+			var _timeDifference = methods.difference(_startDate, _endDate);
+			
+			_timeDifference = methods.formatted(_timeDifference, newOptions);
+			
+			setInterval(function() { methods.update(_timeDifference, newOptions); }, 999);	
 		},
 		
 		difference : function(start, end) {
+
 			var timeDiff = $.extend([], _timeStructure);
-			
 			//year difference
 			timeDiff.year = end.year - start.year;
 			var eYearString = parseInt(String(end.month)+String(end.day)+String(end.hour)+String(end.minute), 10);
@@ -119,23 +134,28 @@
 				timeDiff.seconds = end.seconds - start.seconds;
 			}
 			
+			return timeDiff;		
+		},
+		
+		formatted : function (timeDiff, options) {
 			//format control
-			if(_defaults['format'] != 'full') {
+			if(options['format'] != 'full') {
 				timeDiff.day = (timeDiff.year * 365) + (timeDiff.month * 30) + timeDiff.day;
 				timeDiff.year = 0; 
 				timeDiff.month = 0;
-				if(_defaults['format'] == 'seconds') {
+				if(options['format'] == 'seconds') {
 					timeDiff.seconds = (timeDiff.day*86400) + (timeDiff.hour*3600) + (timeDiff.minute*60) + timeDiff.seconds;
 					timeDiff.day = 0;
 					timeDiff.hour = 0;
 					timeDiff.minute = 0;
 				}
 			}
-			setInterval(function() { methods.update(timeDiff); }, 999);			
+			
+			return timeDiff;			
 		},
 		
-		update : function (time) {
-			if ( time.seconds < 59 || _defaults['format'] == 'seconds' ) time.seconds++;
+		update : function (time, newOptions) {
+			if ( time.seconds < 59 || newOptions['format'] == 'seconds' ) time.seconds++;
 			else { 
 				time.seconds = 0;
 				if ( time.minute < 59 ) time.minute++;
@@ -144,7 +164,7 @@
 					if ( time.hour < 23 ) time.hour++;
 					else {
 						time.hour = 0;
-						if ( time.day < _monthDayStructure[_defaults.expiryDate.getMonth()] || _defaults['format'] == 'day' ) time.day++;
+						if ( time.day < _monthDayStructure[_defaults.expiryDate.getMonth()] || newOptions['format'] == 'day' ) time.day++;
 						else {
 							time.day = 0;
 							if ( time.month < 12 ) time.month++;
@@ -155,71 +175,88 @@
 						}
 					}
 				}
-			}			
-			methods.print(time);	
+			}	
+			methods.print(time, newOptions);	
 		},
 		
-		print : function(time) {
-			var year   = '<span id='+_defaults["class_prefix"]+'_year">'+time.year+' '+_lang[_defaults['lang']].year+_defaults['seperator']+' </span>';
-			var month  = '<span id='+_defaults["class_prefix"]+'_month">'+time.month+' '+_lang[_defaults['lang']].month+_defaults['seperator']+' </span>';
-			var day    = '<span id='+_defaults["class_prefix"]+'_day">'+time.day+' '+_lang[_defaults['lang']].day+_defaults['seperator']+' </span>';
-			var hour   = '<span id='+_defaults["class_prefix"]+'_hour">'+time.hour+' '+_lang[_defaults['lang']].hour+_defaults['seperator']+' </span>';
-			var day    = '<span id='+_defaults["class_prefix"]+'_day">'+time.day+' '+_lang[_defaults['lang']].day+_defaults['seperator']+' </span>';
-			var hour   = '<span id='+_defaults["class_prefix"]+'_hour">'+time.hour+' '+_lang[_defaults['lang']].hour+_defaults['seperator']+' </span>';
-			var minute = '<span id='+_defaults["class_prefix"]+'_minute">'+time.minute+' '+_lang[_defaults['lang']].minute+_defaults['seperator']+' </span>';
-			var seconds = '<span id='+_defaults["class_prefix"]+'_seconds">'+time.seconds+' '+_lang[_defaults['lang']].seconds+' </span>';
+		print : function(time, newOptions) {
 			
-			if(_defaults['write_zero'] == 'true') {
-				$(_defaults['div_id']).html(year+month+day+hour+minute+seconds);
+			var year   = '<span id='+newOptions["class_prefix"]+'_year">'+time.year+' '+_lang[newOptions['lang']].year+newOptions['seperator']+' </span>';
+			var month  = '<span id='+newOptions["class_prefix"]+'_month">'+time.month+' '+_lang[newOptions['lang']].month+newOptions['seperator']+' </span>';
+			var day    = '<span id='+newOptions["class_prefix"]+'_day">'+time.day+' '+_lang[newOptions['lang']].day+newOptions['seperator']+' </span>';
+			var hour   = '<span id='+newOptions["class_prefix"]+'_hour">'+time.hour+' '+_lang[newOptions['lang']].hour+newOptions['seperator']+' </span>';
+			var day    = '<span id='+newOptions["class_prefix"]+'_day">'+time.day+' '+_lang[newOptions['lang']].day+newOptions['seperator']+' </span>';
+			var hour   = '<span id='+newOptions["class_prefix"]+'_hour">'+time.hour+' '+_lang[newOptions['lang']].hour+newOptions['seperator']+' </span>';
+			var minute = '<span id='+newOptions["class_prefix"]+'_minute">'+time.minute+' '+_lang[newOptions['lang']].minute+newOptions['seperator']+' </span>';
+			var seconds = '<span id='+newOptions["class_prefix"]+'_seconds">'+time.seconds+' '+_lang[newOptions['lang']].seconds+' </span>';
+			
+			if(newOptions['write_zero'] == 'true') {
+				$(newOptions['div_id']).html(year+month+day+hour+minute+seconds);
 			} else {
-				$(_defaults['div_id']).html(null);
+				$(newOptions['div_id']).html(null);
 				var zero = false;
 				if((time.year > 0) ||  zero) {
-					$(_defaults['div_id']).append(year);
+					$(newOptions['div_id']).append(year);
 					zero = true;
 				}
 				if((time.month > 0) || zero) {
-					$(_defaults['div_id']).append(month);
+					$(newOptions['div_id']).append(month);
 				}
 				if((time.day > 0) ||  zero) { 
-					$(_defaults['div_id']).append(day);
+					$(newOptions['div_id']).append(day);
 					zero = true;
 				}
 				if((time.hour > 0) ||  zero) {
-					$(_defaults['div_id']).append(hour);
+					$(newOptions['div_id']).append(hour);
 					zero = true;
 				}
 				if((time.minute > 0) ||  zero) {
-					$(_defaults['div_id']).append(minute);
+					$(newOptions['div_id']).append(minute);
 					zero = true;
 				}
 				if((time.seconds > 0) ||  zero)
-					$(_defaults['div_id']).append(seconds);				
+					$(newOptions['div_id']).append(seconds);				
 			}
 		},
-
-		getStartDate : function(sinceDate) {			
-			var splittedExpiryDate = sinceDate.split("-");			
+		
+		getStartDateWithFormat : function(sinceDate) {
+			//split hour and date			
+			var splittedExpiryDate = sinceDate.split("-");
+			
+			//use date
 			var split_expiry_date = splittedExpiryDate[0].split("/");
+
+			//use hour
 			var split_expiry_time = splittedExpiryDate[1].split(":");
-			var startDate = $.extend([], _timeStructure);			
+
+			//set time structure variables
+			var startDate = $.extend([], _timeStructure);
+		
 			startDate.year   = split_expiry_date[2];
 			startDate.month  = split_expiry_date[1];
 			startDate.day    = split_expiry_date[0];
+			
 			startDate.hour   = split_expiry_time[0];
-			startDate.minute = split_expiry_time[1];	
+			startDate.minute = split_expiry_time[1];
+			
+			//if seconds
 			(split_expiry_time[2]) ? startDate.seconds = split_expiry_time[2] : startDate.seconds = 0;			
+
 			return startDate;
 		},
 		
 		getExpiryDate : function(expiryDate) {
+
 			var endDate = $.extend([], _timeStructure);
+
 			endDate.year   = expiryDate.getFullYear();
+
 			(expiryDate.getMonth()<9)    ? endDate.month   = "0" + expiryDate.getMonth()+1 : endDate.month   = expiryDate.getMonth()+1;
 			(expiryDate.getDate()<10)    ? endDate.day     = "0" + expiryDate.getDate()    : endDate.day     = expiryDate.getDate();
 			(expiryDate.getHours()<10)   ? endDate.hour    = "0" + expiryDate.getHours()   : endDate.hour    = expiryDate.getHours();
 			(expiryDate.getMinutes()<10) ? endDate.minute  = "0" + expiryDate.getMinutes() : endDate.minute  = expiryDate.getMinutes();
 			(expiryDate.getSeconds()<10) ? endDate.seconds = "0" + expiryDate.getSeconds() : endDate.seconds = expiryDate.getSeconds();	
+
 			return endDate;
 		},
 	};
@@ -227,13 +264,8 @@
 	$.fn.countUp = function (method) {	
 		_defaults['div_id'] = "#"+$(this).attr('id');
 		_defaults['class_prefix'] = $(this).attr('id');
-		if( methods[method] ) {
-			return methods[method].apply(this, Array.prototype.call(arguments, 1) );
-		} else if ( typeof method == 'object' || !method) {
-			return methods.init.apply(this, arguments);
-		} else {
-			$.error( 'Method: ' + method + ' does not exist on jQuery.countUp');
-		}		
+		var myMethods = Object.create(methods);
+		myMethods.init(method);
 	}
 	
 })( jQuery );
